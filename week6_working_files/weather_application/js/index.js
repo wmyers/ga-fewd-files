25b210c4b8f9af4602a60da9b1193091f3ad987c
WM <wmmyers@hotmail.com>
2016-01-29 16:44:31 +0800

    week6 wip

---
+++ week6_working_files/weather_application/js/index.js
@@ -0,0 +1,34 @@
+//store the breakpoint Celsius value in a variable
+var breakpointCelsius = 20;
+
+//write a Celsius-Fahrenheit converter function
+//T(°F) = T(°C) × 1.8 + 32
+
+function convertToFahrenheit(celsius){
+
+}
+
+//write an image display function for hot or cold images
+//to update the #imageContainer in the HTML
+
+function updateImage(celsius){
+
+}
+
+//write a function to update the result message that is displayed below the image
+//the message template should be:
+// "N°C (N°F). That's (hot OR cold)!"
+
+function updateResultMessage(celsius, fahrenheit){
+
+}
+
+//write a click handler function for the submit button that brings everything together
+
+$("#submit").click(function(){
+
+  
+
+  //we need to return false to stop the page reloading
+  return false;
+});
